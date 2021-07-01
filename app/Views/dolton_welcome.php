e9735b41eb898886c6fd9cad82eba78c0404dba8
Afif Farakhan <afarakhan.af@gmail.com>
2021-07-01 12:36:06 +0700

    commit update 12:53 HOOPS putih

--- app/Views/dolton_welcome.php
+++ app/Views/dolton_welcome.php
@@ -6,9 +6,52 @@
 
 	<div class="mySlides fade">
 		<!-- <div class="numbertext">1 / 3</div> -->
-		<!-- <img src="images/slider2.jpg" style="width:100%"> -->
+		<!-- <img src="assets/images/pexels-skitterphoto-9510.jpg" style="width:100%"> -->
 		<!-- <div class="text">Caption Text</div> -->
 	</div>
+
+	<section class="carousel" aria-label="Gallery">
+		<ol class="carousel__viewport">
+			<li id="carousel__slide1" tabindex="0" class="carousel__slide">
+				<div class="carousel__snapper">
+					<img src="assets/images/pexels-skitterphoto-9510.jpg">
+					<a href="#carousel__slide4" class="carousel__prev">Go to last slide</a>
+					<a href="#carousel__slide2" class="carousel__next">Go to next slide</a>
+				</div>
+			</li>
+			<li id="carousel__slide2" tabindex="0" class="carousel__slide">
+				<div class="carousel__snapper"></div>
+				<a href="#carousel__slide1" class="carousel__prev">Go to previous slide</a>
+				<a href="#carousel__slide3" class="carousel__next">Go to next slide</a>
+			</li>
+			<li id="carousel__slide3" tabindex="0" class="carousel__slide">
+				<div class="carousel__snapper"></div>
+				<a href="#carousel__slide2" class="carousel__prev">Go to previous slide</a>
+				<a href="#carousel__slide4" class="carousel__next">Go to next slide</a>
+			</li>
+			<li id="carousel__slide4" tabindex="0" class="carousel__slide">
+				<div class="carousel__snapper"></div>
+				<a href="#carousel__slide3" class="carousel__prev">Go to previous slide</a>
+				<a href="#carousel__slide1" class="carousel__next">Go to first slide</a>
+			</li>
+		</ol>
+		<aside class="carousel__navigation">
+			<ol class="carousel__navigation-list">
+				<li class="carousel__navigation-item">
+					<a href="#carousel__slide1" class="carousel__navigation-button">Go to slide 1</a>
+				</li>
+				<li class="carousel__navigation-item">
+					<a href="#carousel__slide2" class="carousel__navigation-button">Go to slide 2</a>
+				</li>
+				<li class="carousel__navigation-item">
+					<a href="#carousel__slide3" class="carousel__navigation-button">Go to slide 3</a>
+				</li>
+				<li class="carousel__navigation-item">
+					<a href="#carousel__slide4" class="carousel__navigation-button">Go to slide 4</a>
+				</li>
+			</ol>
+		</aside>
+	</section>
 
 	<!-- Hi! Section --->
 	<div class="container">
@@ -18,8 +61,6 @@
 			</ul>
 		</div>
 	</div>
-
-
 
 	<!-- Best Sellers Section --->
 	<div class="container">

9a05f6f004f5a4b521f83a0b43a3ec956b2dbdf4
Afif Farakhan <afarakhan.af@gmail.com>
2021-07-05 10:56:35 +0700

    update carousels 3

--- app/Views/dolton_welcome.php
+++ app/Views/dolton_welcome.php
@@ -10,48 +10,19 @@
 		<!-- <div class="text">Caption Text</div> -->
 	</div>
 
-	<section class="carousel" aria-label="Gallery">
-		<ol class="carousel__viewport">
-			<li id="carousel__slide1" tabindex="0" class="carousel__slide">
-				<div class="carousel__snapper">
-					<img src="assets/images/pexels-skitterphoto-9510.jpg">
-					<a href="#carousel__slide4" class="carousel__prev">Go to last slide</a>
-					<a href="#carousel__slide2" class="carousel__next">Go to next slide</a>
-				</div>
-			</li>
-			<li id="carousel__slide2" tabindex="0" class="carousel__slide">
-				<div class="carousel__snapper"></div>
-				<a href="#carousel__slide1" class="carousel__prev">Go to previous slide</a>
-				<a href="#carousel__slide3" class="carousel__next">Go to next slide</a>
-			</li>
-			<li id="carousel__slide3" tabindex="0" class="carousel__slide">
-				<div class="carousel__snapper"></div>
-				<a href="#carousel__slide2" class="carousel__prev">Go to previous slide</a>
-				<a href="#carousel__slide4" class="carousel__next">Go to next slide</a>
-			</li>
-			<li id="carousel__slide4" tabindex="0" class="carousel__slide">
-				<div class="carousel__snapper"></div>
-				<a href="#carousel__slide3" class="carousel__prev">Go to previous slide</a>
-				<a href="#carousel__slide1" class="carousel__next">Go to first slide</a>
-			</li>
-		</ol>
-		<aside class="carousel__navigation">
-			<ol class="carousel__navigation-list">
-				<li class="carousel__navigation-item">
-					<a href="#carousel__slide1" class="carousel__navigation-button">Go to slide 1</a>
-				</li>
-				<li class="carousel__navigation-item">
-					<a href="#carousel__slide2" class="carousel__navigation-button">Go to slide 2</a>
-				</li>
-				<li class="carousel__navigation-item">
-					<a href="#carousel__slide3" class="carousel__navigation-button">Go to slide 3</a>
-				</li>
-				<li class="carousel__navigation-item">
-					<a href="#carousel__slide4" class="carousel__navigation-button">Go to slide 4</a>
-				</li>
-			</ol>
-		</aside>
-	</section>
+	<!-- Carousels Section -->
+	<ul class="gallery">
+
+		<li><a><img src="assets/images/cookie1.jpg" /></a></li>
+		<li style="background: #f7ede2;"></li>
+		<!--
+		<li style="background: #f6bd60;"></li>
+		<li style="background: #f7ede2;"></li>
+		<li style="background: #f5cac3;"></li>
+		<li style="background: #84a59d;"></li>
+		<li style="background: #f28482;"></li>
+		-->
+	</ul>
 
 	<!-- Hi! Section --->
 	<div class="container">
